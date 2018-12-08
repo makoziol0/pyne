ac4971cb6acfd4c4359b53c01b9b79d69aa7553d
Matthew Koziol <matthewkoziol0@gmail.com>
2018-12-08 16:59:33 -0600

    Revert "Update reference to new mass file"
    This reverts commit bd42e6ef6babed7ab18cec0b320e11d6eb2a745a.

--- cmake/ReplicatePythonSourceTree.cmake
+++ cmake/ReplicatePythonSourceTree.cmake
@@ -12,6 +12,5 @@ file(COPY setup.py setup_sub.py pyne DESTINATION "${CMAKE_ARGV3}"
                     PATTERN "*.pyi"
                     PATTERN "*.pxd"
                     PATTERN "*.mas12"
-                    PATTERN "*.mas16"
                     PATTERN "*.dat"
                     )
